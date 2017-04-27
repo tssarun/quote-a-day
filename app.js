8436ed27c436691596ece6cdd9716df209d4d17d
Arunkumar Sivasankaran <arunkumar.sivasankaran@mindtree.com>
2017-04-27 11:11:42 +0530

    Adding changes

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var app =express();
+var express=require('express');
+var app=express();
 
-app.get('/',function (request,response) {
-	response.sendFile(__dirname+'/index.html')
+app.get('/',function(request,response){
+  response.sendFile(__dirname+'/index.html');
 });
-var port=process.env.PORT || 8080;
-// Listen on port 8000, IP defaults to 127.0.0.1
 
-var server=app.listen(port,function (req,res) {
-// Put a friendly message on the terminal
-console.log("Server running at http://localhost/"+port);
+var port = process.env.PORT || 8080;
+
+var server=app.listen(port,function(req,res){
+    console.log("Catch the action at http://localhost:"+port);
 });
